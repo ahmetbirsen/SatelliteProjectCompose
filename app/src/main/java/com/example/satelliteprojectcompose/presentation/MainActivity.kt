492c20f9dfd80b3a5664f93b3ff0ff70a84bbafc
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2023-12-07 11:09:49 +0300

    Add navhost into main activity and added SatelliteListScreen here.

--- app/src/main/java/com/example/satelliteprojectcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/example/satelliteprojectcompose/presentation/MainActivity.kt
@@ -1,30 +1,46 @@
 package com.example.satelliteprojectcompose.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.satelliteprojectcompose.presentation.satellites.views.SatelliteScreen
 import com.example.satelliteprojectcompose.presentation.ui.theme.SatelliteProjectComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SatelliteProjectComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.SatelliteListScreen.route ) {
+                        composable(route = Screen.SatelliteListScreen.route) {
+                            SatelliteScreen(navController = navController)
+                        }
+                    }
                 }
             }
         }
