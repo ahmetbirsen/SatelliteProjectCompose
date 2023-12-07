e5ab3a3b2e6539670c0376cdd46f3464241bc514
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2023-12-07 10:24:06 +0300

    SatelliteRepository and its implemenatation created. getSatellites function created.

---
+++ app/src/main/java/com/example/satelliteprojectcompose/data/repository/SatelliteRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.satelliteprojectcompose.data.repository
+
+
+import com.example.projectplayground.data.dto.SatelliteDtoItem
+import com.example.projectplayground.data.file.source.SatelliteJsonSource
+import com.example.satelliteprojectcompose.domain.repository.SatelliteRepository
+import javax.inject.Inject
+
+class SatelliteRepositoryImpl @Inject constructor(
+    private val satellitesJsonSource : SatelliteJsonSource
+
+) : SatelliteRepository {
+    override suspend fun getSatellites() : List<SatelliteDtoItem> {
+        return satellitesJsonSource.getSatellites()
+    }
+
+}
---
+++ app/src/main/java/com/example/satelliteprojectcompose/domain/repository/SatelliteRepository.kt
@@ -0,0 +1,10 @@
+package com.example.satelliteprojectcompose.domain.repository
+
+
+import com.example.projectplayground.data.dto.SatelliteDetailDto
+import com.example.projectplayground.data.dto.SatelliteDtoItem
+import com.example.projectplayground.data.dto.SatellitePosition
+
+interface SatelliteRepository {
+    suspend fun getSatellites(): List<SatelliteDtoItem>
+}
