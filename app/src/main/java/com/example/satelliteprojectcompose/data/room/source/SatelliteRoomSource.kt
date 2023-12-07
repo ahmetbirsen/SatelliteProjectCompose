4255395af4eb30c7e33a32833e04dd8612ad648a
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2023-12-07 11:55:57 +0300

    SatellitesRoom interface source created

---
+++ app/src/main/java/com/example/satelliteprojectcompose/data/room/source/SatelliteRoomSource.kt
@@ -0,0 +1,16 @@
+package com.example.satelliteprojectcompose.data.room.source
+
+import com.example.projectplayground.data.dto.SatelliteDetailDto
+import com.example.projectplayground.data.dto.SatellitePosition
+
+
+interface SatelliteRoomSource{
+    suspend fun getSatelliteDetail(satelliteId: Int): SatelliteDetailDto?
+    suspend fun insertSatelliteDetail(satelliteDetail: SatelliteDetailDto)
+    suspend fun deleteSatelliteDetails(satelliteDetail: SatelliteDetailDto)
+
+    suspend fun getSatellitePositions(satelliteId: String) : SatellitePosition?
+    suspend fun insertSatellitePositions(satellitePosition: SatellitePosition)
+    suspend fun deleteSatellitePositions(satellitePosition: SatellitePosition)
+
+}
