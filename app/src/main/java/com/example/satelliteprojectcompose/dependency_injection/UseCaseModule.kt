155dbcbb22ea348c0003b273978fc24dc6136970
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2023-12-07 10:40:52 +0300

    UseCaseModule created and provides GetSatelliteUseCase process.

---
+++ app/src/main/java/com/example/satelliteprojectcompose/dependency_injection/UseCaseModule.kt
@@ -0,0 +1,25 @@
+package com.example.satelliteprojectcompose.dependency_injection
+
+import com.example.projectplayground.domain.use_case.get_satellites.GetSatellitesUseCase
+import com.example.satelliteprojectcompose.domain.repository.SatelliteRepository
+import com.example.satelliteprojectcompose.domain.use_case.SatelliteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+object UseCaseModule {
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object UseCaseModule {
+
+        @Provides
+        @Singleton
+        fun providesSatelliteUseCases(repository: SatelliteRepository): SatelliteUseCases {
+            return SatelliteUseCases(
+                getSatellite = GetSatellitesUseCase(repository)
+            )
+        }
+    }
+}
